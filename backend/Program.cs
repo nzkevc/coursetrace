568b9fb8b8dd9106f64091a44b0d906a6b821db9
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-06-23 23:50:56 +1200

    feat: configure CORS for localhost (frontend)

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,10 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+        .AllowAnyHeader()
+        .AllowAnyMethod(); // Allow CORS requests from localhost
+    });
+});
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer(); // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
